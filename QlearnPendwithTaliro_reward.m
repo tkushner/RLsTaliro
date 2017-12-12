2679d36ab417505caa31d79e002671c14c2d789c
Taisa Kushner <taisa.kushner@colorado.edu>
2017-12-12 12:32:12 -0700

    merge error, fixing with duplicates

--- QlearnPendwithTaliro_reward.m
+++ QlearnPendwithTaliro_reward.m
@@ -87,10 +87,23 @@ model2 = @(t,x) [x(2); g/l*sin(x(1))];
 x=XT2(:,1);
 v=XT2(:,2);
 
+% Write to video?
+doVid = true;
+
+if doVid
+    writerObj = VideoWriter('sTaliro.mp4','MPEG-4');
+    writerObj.FrameRate = 60;
+    open(writerObj);
+end
+
 cartesianx=-sin(x);
 cartesiany=cos(x);
 for i = 1 : tEnd
+<<<<<<< HEAD
     figure(6)
+=======
+    panel = figure(2);
+>>>>>>> d4f9ad08414a0702530c0bdccd5255c2626171d0
     subplot(2,1,1)
     plotarrayx = [0 cartesianx(i)];
     plotarrayy = [0 cartesiany(i)];
@@ -108,16 +121,29 @@ for i = 1 : tEnd
         ylabel('velocity','fontsize',12)
         hold on  % Holds previous values
         axis([0 i+1 min(v)-1 max(v)+1])
+    
+    if doVid
+        frame = getframe(panel);
+        writeVideo(writerObj,frame);
+    end    
+    
     pause(.1)  % Shows results at each time interval
     movie(i)=getframe(gcf);
 end
 %%
 v = VideoWriter('Taliro.avi');
 
+<<<<<<< HEAD
 open(v)
 writeVideo(v,movie)
 
 close(v)
+=======
+if doVid
+    close(writerObj);
+end
+
+>>>>>>> d4f9ad08414a0702530c0bdccd5255c2626171d0
 %% RL with Taliro
 %% SETTINGS
 
@@ -161,11 +187,8 @@ actions = [0, -tLim, tLim]; % Only 3 options, Full blast one way, the other way,
 % we see the reward function underneath.
 transpMap = true;
 
-% Write to video?
-doVid = false;
-
 if doVid
-    writerObj = VideoWriter('qlearnVid.mp4','MPEG-4');
+    writerObj = VideoWriter('QlearnPend.mp4','MPEG-4');
     writerObj.FrameRate = 60;
     open(writerObj);
 end
@@ -216,6 +239,7 @@ V = zeros(size(states,1),1);
 Vorig = reshape(max(Q,[],2),[length(x2),length(x1)]);
 
 % Set up the pendulum plot
+clearvars panel;
 panel = figure;
 panel.Position = [680 558 1000 400];
 panel.Color = [1 1 1];
@@ -374,6 +398,10 @@ for episodes = 1:maxEpi
                 set(map,'AlphaData',fullV~=Vorig); % Some spots have not changed from original. If not, leave them transparent.
             end
             drawnow;
+            
+            if ~ishandle(panel)
+                break;
+            end
             
             % Take a video frame if turned on.
             if doVid
